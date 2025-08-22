8c50d4e0306ab144b82143cf3697dd2d4edd1770
lavinse100 <119337552+lavinse100@users.noreply.github.com>
2025-08-22 12:15:13 -0400

    stop if inst login

--- js/viewit-styles.module.js
+++ js/viewit-styles.module.js
@@ -10,13 +10,15 @@ angular
 
         if (isGES && services) {
           const viewitContainer = document.querySelector('prm-alma-viewit');
+          const keys = Object.keys(services);
 
-          Object.keys(services).forEach(key => {
+          keys.some(key => {
             let packageName = services[key].packageName.toLowerCase();
-            let isInstLogin = packageName.includes('access') || packageName.includes('log in to') || packageName.includes('watch');
+            let isInstLogin = packageName.includes('access') || packageName.includes('log in') || packageName.includes('login') || packageName.includes('watch');
             if (isInstLogin) {
               viewitContainer.classList.remove('bc-viewit-item');
               viewitContainer.classList.add('inst-login');
+              return true;
             } else {
               viewitContainer.classList.add('bc-viewit-item');
               viewitContainer.classList.remove('inst-login');
